3c95b516f5dafd9d46557455ecd0121111ed6729
Daniil Trukhanov <trukhanovdaniil@gmail.com>
2026-06-11 14:49:12 +0500

    Minor bug fix

--- practice1/strings.py
+++ practice1/strings.py
@@ -127,3 +127,4 @@ print(txt)
 
 #Escape Character
 txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
---
+++ practice2/bool.py
@@ -0,0 +1,25 @@
+#ex1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#ex2
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#evaluate values and variables
+#ex1
+print(bool("Hello"))
+print(bool(15))
+
+#ex2
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
