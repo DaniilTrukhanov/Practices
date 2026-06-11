98518807e47d7f3e28bd98e2d31eeb7fbe74a8a4
Daniil Trukhanov <trukhanovdaniil@gmail.com>
2026-06-11 14:33:54 +0500

    Add Practice1 Syntax examples

---
+++ practice1/syntax/indentation.py
@@ -0,0 +1,9 @@
+#Example 1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Example 2
+if 5 > 2:
+  print ("Five is greater than two!")
+if 5 > 2:
+    print ("Five is greater than two!")
---
+++ practice1/syntax/statements.py
@@ -0,0 +1,1 @@
+print ("pythin is fun!")
