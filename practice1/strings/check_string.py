b90d6a50bd996dd0b145b7f9f5dae51e49f8d816
Daniil Trukhanov <trukhanovdaniil@gmail.com>
2026-06-11 14:20:04 +0500

    Add Practice1 Strings examples

---
+++ practice1/strings/check_string.py
@@ -0,0 +1,8 @@
+#example 1
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#example 2
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
---
+++ practice1/strings/placeholders_and_modifiers.py
@@ -0,0 +1,11 @@
+#Example 1
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+#Example 2
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+#Example 3
+txt = f"The price is {20 * 59} dollars"
+print(txt)
