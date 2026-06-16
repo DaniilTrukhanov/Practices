1ba9275bd4aeea0f91fa630286d5f2fbdddaab7d
Daniil Trukhanov <trukhanovdaniil@gmail.com>
2026-06-16 10:21:30 +0500

    Practice 3 lambda examples

---
+++ practice3/lambda/basics.py
@@ -0,0 +1,19 @@
+#ex1
+x = lambda a : a + 10
+print(x(5))
+
+#ex2
+x = lambda a, b : a * b
+print(x(5, 6))
+
+#ex3
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#ex4
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
